322d4aeb8b4cb3d1a060a95568d34cdafb61e7d1
Ryan Dahl <ry@tinyclouds.org>
2018-06-16 01:43:23 +0200

    First pass at deno rust

---
+++ deno2/main.rs
@@ -0,0 +1,16 @@
+extern crate libc;
+use std::ffi::CStr;
+
+#[link(name = "deno")]
+extern {
+    fn deno_v8_version() -> *const libc::c_char;
+    //fn deno_init();
+}
+
+fn main() {
+    //unsafe { deno_init() };
+    let v = unsafe { deno_v8_version() };
+    let c_str = unsafe { CStr::from_ptr(v) };
+    let version = c_str.to_str().unwrap();
+    println!("version: {}", version);
+}
